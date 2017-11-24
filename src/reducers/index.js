0bbc1863133c0c0bf2630ffa70eb0ed349f92537
José Paradas <jparadas.b@gmail.com>
2017-11-24 13:01:26 -0300

    Configure gh-pages

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import ContactsListReducer from './contacts-list-reducer';
-
-const rootReducer = combineReducers({
-  contactsListState: ContactsListReducer,
-});
-
-export default rootReducer;
